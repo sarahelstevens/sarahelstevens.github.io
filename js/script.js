76224dbd977c8de9c97d70b101afc27b32265683
sarahelstevens <sarah.el.stevens@gmail.com>
2023-01-06 11:45:29 +1100

    Updating folder structure

--- js/script.js
+++ js/script.js
@@ -9,11 +9,11 @@ var h = window.innerHeight;
 
 function toggleMobileNav(){
     $('nav, .mobile-nav').toggleClass('open');
-    $('.mobile-nav').text('Close').css('color', '#FDB2B5');
+    $('.mobile-nav').text('Close').css('color' ,'#000').css('font-weight' , '500');
     $('nav.open').css('height', h);
 
     if ($('.mobile-nav').hasClass('open') === false) {
-    	$('.mobile-nav').text('Menu').css('color', '#333');
+    	$('.mobile-nav').text('Menu').css('color' ,'#3B0CD2').css('font-weight' , '500');
     	$('nav').css('height', nav);
     }
   }
